826fc391a29d768f7b58f517ba5c7df3f8542602
Alex Costantini <alexcostantini@alexcostantini.local>
2017-01-05 15:09:54 -0500

    fixed supplier profile picture migration

--- db/migrate/20150128193737_add_profile_picture_to_spree_suppliers.rb
+++ db/migrate/20150128193737_add_profile_picture_to_spree_suppliers.rb
@@ -1,4 +1,4 @@
-class AddProfilePictureColumnsToSpreeSuppliers < ActiveRecord::Migration
+class AddProfilePictureToSpreeSuppliers < ActiveRecord::Migration
   def change
 		add_attachment :spree_suppliers, :profile_picture
 	end
