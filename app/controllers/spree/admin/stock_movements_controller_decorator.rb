3b41c8a26e72d7ad8056ce052bef0f92b81566be
Alex Costantini <alexcostantini@alexcostantini.local>
2017-01-26 13:59:01 -0500

    changed back to profile redirect

--- app/controllers/spree/admin/stock_movements_controller_decorator.rb
+++ app/controllers/spree/admin/stock_movements_controller_decorator.rb
@@ -1,10 +1,10 @@
 Spree::Admin::StockMovementsController.class_eval do
 
-  before_filter :redirect_to_products
+  before_filter :redirect_to_profile
 
-  def redirect_to_products
+  def redirect_to_profile
   	if try_spree_current_user.try(:supplier?)
-    	redirect_to spree.admin_products_path
+    	redirect_to spree.edit_admin_supplier_url(spree_current_user.supplier_id)
     end
   end
   
