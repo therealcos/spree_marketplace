501c929f793e03cbb8bc7fb9ad52fc55805d76a6
Alex Costantini <alexcostantini@alexcostantini.local>
2017-01-26 13:25:56 -0500

    added redirect to products for stock locations

---
+++ app/controllers/spree/admin/stock_locations_controller_decorator.rb
@@ -0,0 +1,9 @@
+Spree::Admin::StockLocationsController.class_eval do
+
+  before_filter :redirect_to_products
+
+  def redirect_to_products
+    redirect_to spree.admin_products_path
+  end 
+  
+end
