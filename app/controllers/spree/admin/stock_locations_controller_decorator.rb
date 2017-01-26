3d46d85ceb2f4606157e5d27bb6614290a0dc5f3
Alex Costantini <alexcostantini@alexcostantini.local>
2017-01-26 13:40:44 -0500

    fixed redirects to products

--- app/controllers/spree/admin/stock_locations_controller_decorator.rb
+++ app/controllers/spree/admin/stock_locations_controller_decorator.rb
@@ -3,7 +3,9 @@ Spree::Admin::StockLocationsController.class_eval do
   before_filter :redirect_to_products
 
   def redirect_to_products
-    redirect_to spree.admin_products_path
+  	if try_spree_current_user.try(:supplier?)
+    	redirect_to spree.admin_products_path
+    end
   end 
   
 end
