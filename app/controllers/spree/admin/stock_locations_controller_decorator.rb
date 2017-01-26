01bc0b205d6a488c599c3433ab642495ad779a79
Alex Costantini <alexcostantini@alexcostantini.local>
2017-01-26 13:55:34 -0500

    changed stock locations redirect back to products

--- app/controllers/spree/admin/stock_locations_controller_decorator.rb
+++ app/controllers/spree/admin/stock_locations_controller_decorator.rb
@@ -1,10 +1,10 @@
 Spree::Admin::StockLocationsController.class_eval do
 
-  before_filter :redirect_to_profile
+  before_filter :redirect_to_products
 
-  def redirect_to_profile
+  def redirect_to_products
   	if try_spree_current_user.try(:supplier?)
-    	redirect_to spree.edit_admin_supplier_path
+    	redirect_to spree.admin_products_path
     end
   end 
   
