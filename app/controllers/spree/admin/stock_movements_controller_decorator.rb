f00663838babed46f3066ce4de926bd73cef43d7
Alex Costantini <alexcostantini@alexcostantini.local>
2017-01-26 13:50:27 -0500

    changed stock location redirect to profile

--- app/controllers/spree/admin/stock_movements_controller_decorator.rb
+++ app/controllers/spree/admin/stock_movements_controller_decorator.rb
@@ -1,8 +1,8 @@
 Spree::Admin::StockMovementsController.class_eval do
 
-  before_filter :redirect_to_products
+  before_filter :redirect_to_profile
 
-  def redirect_to_products
+  def redirect_to_profile
   	if try_spree_current_user.try(:supplier?)
     	redirect_to spree.admin_products_path
     end
