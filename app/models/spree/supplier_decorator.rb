749e1586ccc37198f36f7e2d17f62f7c72c58af3
Alex Costantini <alexcostantini@alexcostantini.fios-router.home>
2017-05-18 13:56:28 -0400

    wip

--- app/models/spree/supplier_decorator.rb
+++ app/models/spree/supplier_decorator.rb
@@ -3,10 +3,8 @@ Spree::Supplier.class_eval do
 	attr_accessor :first_name, :last_name
   
 	has_many :bank_accounts, class_name: 'Spree::SupplierBankAccount'
-	has_attached_file :profile_picture, dependent: :destroy, :styles => {:medium => "300x300>", :thumb => "100x100>"}, :path => ":rails_root/public/assets/profile_pictures/:style/:filename", :url => "/assets/profile_pictures/:style/:filename"
+	has_attached_file :profile_picture, dependent: :destroy, :styles => {:medium => "300x300>", :thumb => "100x100>"}, :path => ":rails_root/public/spree/suppliers/:id/:style/:filename", :url => "/spree/suppliers/:id/:style/:filename"
   validates_attachment :profile_picture, :content_type => { :content_type => /\Aimage\/.*\Z/ }, :size => { :in => 0..500.kilobytes }
-
-  validates :tax_id, length: { is: 9, allow_blank: true }
 
   before_create :assign_name
 
