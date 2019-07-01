69efdebc591696916ab5a6215101f308cb89f292
Alex Costantini <therealcos@gmail.com>
2019-07-01 12:35:27 -1000

    wip

--- lib/spree_marketplace/engine.rb
+++ lib/spree_marketplace/engine.rb
@@ -23,7 +23,6 @@ module SpreeMarketplace
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
-      Spree::Ability.register_ability(Spree::MarketplaceAbility)
     end
 
     def self.ckeditor_available?
